3eceeedac6218cd74cd4e9e6457e0f3653655261
Helen <90055337+Manan6669@users.noreply.github.com>
2021-11-18 22:46:39 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace лаба5
+{
+    class Programm
+    {
+        interface IUps
+        {
+            void Break();
+            void Sale();
+        }
+        static void Main(string[] args)
+        {
+
+            Car car = new Car(5689, 2009, 102);
+            Console.WriteLine(car.ToString());
+            Train train = new Train(562, 1992, 3);
+            Console.WriteLine(train.ToString());
+            Engine engine = new Engine(864318, 1998, 120, 2009);
+            Console.WriteLine(engine.ToString());
+            Express express = new Express(879, 2020, 6, 360);
+            Console.WriteLine(express.ToString());
+            express.Death();
+            Carriage carriage = new Carriage(622, 1980, 3, 50, 250);
+            Console.WriteLine(carriage.ToString());
+
+            /*
+            Random rand = new Random();
+            Transport[] arr = new Transport[5];
+            for(int i = 0; i < arr.Length; i++)
+            {
+                switch(rand.Next(5))
+                {
+                    case 0:
+                        {
+                            arr[0] = new Car(5689, 2009, 102);
+                        break; }
+                        
+                   case 1:
+                        {arr[1] = new Train(562, 1992, 3);
+                        break; }
+                        
+                    case 2:
+                        {arr[2] = new  Engine(864318, 1998, 120, 2009);
+                        break; }
+                        
+                    case 3:
+                        { arr[3] = new Express(879, 2020, 6, 360);
+                        break;}
+                        
+                    case 4:
+                        {arr[4] = new Carriage(622, 1980, 3, 250, 50);
+                        break; }
+                        
+                }
+            }
+          
+
+            foreach (var transport in arr)
+                {
+                    transport.Info();
+                }
+            }*/
+
+            List<Transport> list = new List<Transport> { new Train(562, 1992, 3), new Car(5689, 2009, 102) };
+
+            Printer printer = new Printer();
+            foreach (Transport v in list)
+            {
+                Console.WriteLine(printer.IAmPrinting(v));
+            }
+
+            Composition composition = new Composition();
+
+            
+
+            
+            composition.cp.Add(engine);
+            composition.cp.Add(express);
+            composition.cp.Add(carriage);
+
+            composition.GetTr("car");
+            composition.GetTransport(2);
+             //Controller controller = new Controller();
+        }
+
+        public partial class Composition
+        {
+            public List<object> cp = new System.Collections.Generic.List<object>();
+            public void GetTr(string trans)
+            {
+                foreach (Transport v in cp)
+                {
+                    //Console.WriteLine(v.Transport);
+                }
+            }
+            public void GetTransport(int num)
+            {
+                string buf;
+                for (int i = 0; i < cp.Count; i++)
+                {
+                    buf = Convert.ToString(cp[i]);
+                    if (buf.Contains(Convert.ToString(num)))
+                    { Console.WriteLine(buf); }
+                }
+            }
+        
+
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace лаба5
+{ /* Автомобиль, Поезд, Транспортное средство, Экспресс, Двигатель, 
+Вагон.*/ 
+
+   /* enum Transp
+    {
+        Lastochka, Mersedes
+    }*/
+    public abstract  class Transport
+    {
+
+        public int Number;
+        public int Year;
+        
+        protected Transport() { }
+
+        protected Transport (int number, int year)
+        {
+            Number = number;
+            Year = year;
+        }
+
+        public override string ToString()
+        {
+            return "Транспорт " + Number + " собранный в  " + Year + "году";
+        }
+        public abstract bool DoClone();
+    }
+     class Train : Transport//, IClonable
+    {
+        public override bool DoClone()
+        {
+            return true;
+        }
+        public int Сarriages;
+        public Train() { }
+        public Train(int Number, int Year, int Carriages)
+        {
+            this.Number = Number;
+            this.Year = Year;
+            this.Сarriages = Carriages;
+            
+        }
+
+        public override string ToString()
+        {
+            return "Поезд номер " + Number + " собранный в  " + Year + " году содержит " + Сarriages + "вагонов";
+        }
+    }
+
+    class Car : Transport
+    {
+        public override bool DoClone()
+        {
+            return true;
+        }
+        
+        public Car() { }
+        public Car(int Number, int Year, int Speed)
+        {
+            this.Number = Number;
+            this.Year = Year;
+            this.Speed = Speed;
+
+        }
+        public double Speed;
+        public override string ToString()
+        {
+            return " Машина номер " + Number + " собранная в  " + Year + " имеет скорость " + Speed ;
+        }
+    }
+     class Engine: Car
+    {
+        public override bool DoClone()
+        {
+            return true;
+        }
+        public double Power;
+
+        public Engine() { }
+        public Engine(int Number, int Year, int Speed, int Power)
+        {
+            this.Number = Number;
+            this.Year = Year;
+            this.Speed = Speed;
+            this.Power = Power;
+
+
+        }
+
+        public override string ToString()
+        {
+            return " Машина номер " + Number + " собранная в  " + Year + "  имеет скорость " + Speed + " Имеет двигатель силой" + Power;
+        }
+    }
+     class Express: Train
+    {
+        public void Death()
+        {
+            Console.WriteLine("Вас сбили, попробуйте заново");
+        }
+        public override bool DoClone()
+        {
+            return true;
+        }
+        public double Speed;
+
+        public Express() { }
+        public Express(int Number, int Year, int Сarriages, int Speed )
+        {
+            this.Number = Number;
+            this.Year = Year;
+            this.Сarriages = Сarriages;
+            this.Speed = Speed;
+
+
+        }
+        public override string ToString()
+        {
+            return " Поезд номер " + Number + " собранный в  " + Year + " имеет  " + Сarriages + " вагонов и скорость " + Speed;
+        }
+    }
+    //partial
+     sealed class Carriage : Express
+    {
+        public override bool DoClone()
+        {
+            return false;
+        }
+        public  int Seats;
+        public Carriage() { }
+        public Carriage(int Number, int Year, int Сarriages, int Speed, int Seats)
+        {
+            this.Number = Number;
+            this.Year = Year;
+            this.Сarriages = Сarriages;
+            this.Speed = Speed;
+            this.Seats = Seats;
+        }
+
+        public override string ToString()
+        {
+            return " Поезд номер " + Number + " собранный в  " + Year + " имеет  " + Сarriages + " вагонов, мест " + Seats  + " и скорость " + Speed ;
+        }
+    }
+       /* 
+    }*/
+    
+    interface IClonable
+    {
+        bool DoClone();
+    }
+     class Printer
+    {
+        public string IAmPrinting(Transport someobj)
+        {
+            return someobj.ToString();
+        }
+        public string IAmPrinting(Car someobj)
+        {
+            return someobj.ToString();
+        }
+        public string IAmPrinting(Train someobj)
+        {
+            return someobj.ToString();
+        }
+    }
+
+    
+
+
+    
+}
+
